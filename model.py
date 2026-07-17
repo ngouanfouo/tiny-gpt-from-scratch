2955009d41571405e1e93fc37ddfe84a54bfed5a
Durel-J <132607998+ngouanfouo@users.noreply.github.com>
2026-07-17 18:01:52 +0100

    Step 46: loop_fill_counts

--- model.py
+++ model.py
@@ -382,8 +382,18 @@ def allocate_count_matrix(vocab_size):
     # TODO: return a (vocab_size, vocab_size) integer array of zeros.
     return np.zeros((vocab_size,vocab_size),dtype=np.int64)
 
-# Step 46 - loop_fill_counts (not yet solved)
-# TODO: implement
+# Step 46 - loop_fill_counts
+import numpy as np
+
+def loop_fill_counts(n_matrix, data):
+    """Increment n_matrix[curr, next] for every consecutive pair in data."""
+    # TODO: walk consecutive (current, next) pairs in data and add 1 to the matching cell
+    for t in range(len(data)-1):
+        curr_token=data[t]
+        next_token=data[t+1]
+
+        n_matrix[curr_token,next_token]+=1
+    return n_matrix
 
 # Step 47 - vectorize_counts_add_at (not yet solved)
 # TODO: implement
