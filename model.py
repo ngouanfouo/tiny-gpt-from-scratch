769df6f00d794c768fd62a9cd3a732da084479b5
Durel-J <132607998+ngouanfouo@users.noreply.github.com>
2026-07-17 18:37:39 +0100

    Step 58: scale_w_small

--- model.py
+++ model.py
@@ -495,8 +495,13 @@ def initialize_w_random(vocab_size, rng):
     # TODO: sample a (vocab_size, vocab_size) array of standard normal values using rng
     return rng.standard_normal((vocab_size,vocab_size),dtype=np.float64)
 
-# Step 58 - scale_w_small (not yet solved)
-# TODO: implement
+# Step 58 - scale_w_small
+import numpy as np
+
+def scale_w_small(w_matrix, scale):
+    """Return w_matrix scaled by the given small factor."""
+    # TODO: return a new array equal to w_matrix multiplied by scale
+    return w_matrix * scale
 
 # Step 59 - one_hot_encode_batch (not yet solved)
 # TODO: implement
