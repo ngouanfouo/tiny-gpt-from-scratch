0718e5cdb3d7c6cfdcab4daa9deb9b1894f88c27
Durel-J <132607998+ngouanfouo@users.noreply.github.com>
2026-07-17 18:36:07 +0100

    Step 57: initialize_w_random

--- model.py
+++ model.py
@@ -487,8 +487,13 @@ def average_nll(p_matrix, data):
 
     return float(total_nll/num_pairs)
 
-# Step 57 - initialize_w_random (not yet solved)
-# TODO: implement
+# Step 57 - initialize_w_random
+import numpy as np
+
+def initialize_w_random(vocab_size, rng):
+    """Return a (vocab_size, vocab_size) float64 matrix of N(0,1) samples drawn from rng."""
+    # TODO: sample a (vocab_size, vocab_size) array of standard normal values using rng
+    return rng.standard_normal((vocab_size,vocab_size),dtype=np.float64)
 
 # Step 58 - scale_w_small (not yet solved)
 # TODO: implement
