7b95c8a48092f8d6cee040a5aca32c54084f0b9b
Durel-J <132607998+ngouanfouo@users.noreply.github.com>
2026-07-17 17:32:23 +0100

    Step 32: stable_softmax_1d

--- model.py
+++ model.py
@@ -249,8 +249,17 @@ def softmax_overflow_demo(large_value):
         'overflowed':np.isinf(val)
     }
 
-# Step 32 - stable_softmax_1d (not yet solved)
-# TODO: implement
+# Step 32 - stable_softmax_1d
+import numpy as np
+
+def stable_softmax_1d(logits):
+    """Numerically stable softmax over a 1D logits vector."""
+    # TODO: subtract the max before exponentiating, then normalize.
+    max_val=max_along_axis(logits,axis=0)
+
+    shifted_logits=logits-max_val
+    exps=array_exp(shifted_logits)
+    return exps/sum_all(exps)
 
 # Step 33 - stable_softmax_2d_rowwise (not yet solved)
 # TODO: implement
