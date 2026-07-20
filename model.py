4cfe95322b388206920c499eb29bf6394ee77b2e
Durel-J <132607998+ngouanfouo@users.noreply.github.com>
2026-07-20 18:50:39 +0100

    Step 127: get_multihead_output_sequence_length

--- model.py
+++ model.py
@@ -1789,8 +1789,16 @@ def transpose_heads_to_back(x_heads):
     
     return y
 
-# Step 127 - get_multihead_output_sequence_length (not yet solved)
-# TODO: implement
+# Step 127 - get_multihead_output_sequence_length
+def get_multihead_output_sequence_length(x_heads_back):
+    """Return T from a (B, T, n_heads, d_head) tensor."""
+    # TODO: read the sequence-length dimension from x_heads_back's shape
+    
+    # Get the shape of the input
+    shape = get_array_shape(x_heads_back)
+    
+    # Return the second dimension (T)
+    return shape[1]
 
 # Step 128 - merge_heads_to_d_model (not yet solved)
 # TODO: implement
